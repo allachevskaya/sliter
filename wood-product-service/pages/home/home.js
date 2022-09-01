13d595e1cff491e86989a0466362af8d1799df88
allachevskaya <al.lachevskaya33@gmail.com>
2022-09-01 15:46:27 +0300

    /

--- wood-product-service/pages/home/home.js
+++ wood-product-service/pages/home/home.js
@@ -1,11 +1,28 @@
-console.log(2)
 
 const dropdown = document.querySelector(".dropdown");
 const menuItems = document.querySelectorAll(".menu-item")
 
 
 dropdown.addEventListener('click', () => {
-for(let i=0;i<menuItems.length;i++){
-  menuItems[i].classList.toggle('show')
-}
+  for (let i = 0; i < menuItems.length; i++) {
+    menuItems[i].classList.toggle('show')
+  }
+});
+
+
+const navBtns = document.querySelectorAll('.nav__btn')
+
+
+navBtns.forEach((btn) => {
+  btn.addEventListener('click',()=>{
+    clearActiveClass()
+    btn.classList.add('nav__btn__active')
+  })
 })
+
+
+function clearActiveClass() {
+  navBtns.forEach((btn) => {
+    btn.classList.remove('nav__btn__active')
+  })
+}
